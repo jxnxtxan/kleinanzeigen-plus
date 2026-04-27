512936590f81a887eda69b8081eef141a5fc203a
Jonathan <jonathan567835@gmail.com>
2026-04-27 14:22:52 +0200

    Update version number to 1.0.3 in kleinazeigen-plus.js

--- kleinazeigen-plus.js
+++ kleinazeigen-plus.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kleinanzeigen Plus
 // @namespace    https://local.kleinanzeigen.enhanced
-// @version      1.0.2
+// @version      1.0.3
 // @description  Floating settings button with default sort automation.
 // @match        https://www.kleinanzeigen.de/*
 // @homepageURL  https://github.com/jxnxtxan/kleinanzeigen-plus
